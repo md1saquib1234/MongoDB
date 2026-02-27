edc106b00179bb895249d49daab4bc9a97488802
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-02-27 20:01:19 +0530

    feat: implement chat model and initial chat data insertion

--- Mongo3/index.js
+++ Mongo3/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
+const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -17,6 +18,15 @@ async function main() {
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
+
+let chat1 = new Chat({
+  from : "neha",
+  to : "priya",
+  msg : "send me you exam sheets",
+  created_at: new Date()
+});
+
+chat1.save().then(res => {console.log(res)})  
 
 app.get("/", (req, res) => {
   res.send("root is working");
---
+++ Mongo3/init.js
@@ -0,0 +1,50 @@
+const mongoose = require("mongoose");
+const chat = require("./models/chat");
+
+main()
+.then(() => {
+  console.log("connection successful")
+})
+.catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+let allChats = [
+  {
+    from: "neha",
+    to: "preeti",
+    msg : "send me notes for the exam",
+    created_at : new Date(),
+  },
+  {
+    from: "rohit",
+    to: "mohit",
+    msg : "teach me JS callbacks",
+    created_at : new Date(),
+  },
+  {
+    from: "amit",
+    to: "sumit",
+    msg : "all the best",
+    created_at : new Date(),
+  },
+  {
+    from: "anita",
+    to: "ramesh",
+    msg : "bring me some fruits",
+    created_at : new Date(),
+  },
+  {
+    from: "tony",
+    to: "peeter",
+    msg : "love you 3000",
+    created_at : new Date(),
+  },
+  
+]
+
+chat.insertMany(allChats);
