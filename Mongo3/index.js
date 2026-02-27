dd777f88008de1fcea72164fcded89844930230b
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-02-27 22:29:17 +0530

    feat: add chat view and styles for displaying chat messages

--- Mongo3/index.js
+++ Mongo3/index.js
@@ -6,6 +6,7 @@ const Chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
 
 main()
 .then(() => {
@@ -19,14 +20,28 @@ async function main() {
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
-let chat1 = new Chat({
-  from : "neha",
-  to : "priya",
-  msg : "send me you exam sheets",
-  created_at: new Date()
+
+//Index Route
+app.get("/chats", async (req, res) => {
+  let chats = await Chat.find();
+  console.log(chats);
+  res.render("index.ejs", {chats});
 });
 
-chat1.save().then(res => {console.log(res)})  
+
+app.get("/" , (req, res) => {
+  res.send("root is working");
+});
+
+
+// let chat1 = new Chat({
+//   from : "neha",
+//   to : "priya",
+//   msg : "send me you exam sheets",
+//   created_at: new Date()
+// });
+
+// chat1.save().then(res => {console.log(res)})  
 
 app.get("/", (req, res) => {
   res.send("root is working");
