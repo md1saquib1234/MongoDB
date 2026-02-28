89b95728e95446c9b76097b900e4d44b99741294
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-02-28 15:37:33 +0530

    Updating the route by changing the message

--- Mongo3/index.js
+++ Mongo3/index.js
@@ -4,11 +4,13 @@ const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
 const chat = require("./models/chat.js");
+const methodOverride = require("method-override");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({extended: true}));
+app.use(methodOverride("_method"));
 
 main()
 .then(() => {
@@ -26,7 +28,7 @@ async function main() {
 //Index Route
 app.get("/chats", async (req, res) => {
   let chats = await Chat.find();
-  console.log(chats);
+  // console.log(chats);
   res.render("index.ejs", {chats});
 });
 
@@ -59,6 +61,19 @@ app.get("/chats/:id/edit", async (req, res) => {
   let {id} = req.params;
   let chat = await Chat.findById(id);
   res.render("edit.ejs", {chat} );
+});
+
+//Update route
+app.put("/chats/:id", async (req, res) => {
+   let {id} = req.params;
+   let {msg : newMsg} = req.body;
+   console.log(newMsg);
+   let updatedChat  = await chat.findByIdAndUpdate(id, {msg: newMsg},
+     {runValidators: true, new: true} 
+    );
+
+    console.log(updatedChat);
+    res.redirect("/chats");
 });
 
 
