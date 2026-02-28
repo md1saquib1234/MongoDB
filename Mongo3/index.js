7f96ffd296f59054858d14a401d12aba3aad0af4
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-02-28 13:50:21 +0530

    Edit the route

--- Mongo3/index.js
+++ Mongo3/index.js
@@ -3,6 +3,7 @@ const app = express();
 const mongoose = require("mongoose");
 const path = require("path");
 const Chat = require("./models/chat.js");
+const chat = require("./models/chat.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -52,6 +53,14 @@ app.post("/chats", (req, res) => {
 
   res.redirect("/chats");
 });
+
+//edit route
+app.get("/chats/:id/edit", async (req, res) => {
+  let {id} = req.params;
+  let chat = await Chat.findById(id);
+  res.render("edit.ejs", {chat} );
+});
+
 
 
 app.get("/" , (req, res) => {
