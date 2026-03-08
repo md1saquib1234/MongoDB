30bf182cde48c23e5d8ce154a5b1106c880d8951
md1saquib1234 <mdshaanansari0786@gmail.com>
2026-03-09 00:24:30 +0530

    Mongoose error

--- Mongo3/index.js
+++ Mongo3/index.js
@@ -5,6 +5,7 @@ const path = require("path");
 const Chat = require("./models/chat.js");
 const chat = require("./models/chat.js");
 const methodOverride = require("method-override");
+const ExpressError = require("./ExpressError.js");
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
@@ -19,52 +20,77 @@ main()
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+  await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
 
 
 //Index Route
-app.get("/chats", async (req, res) => {
+app.get("/chats", async (req, res, next) => {
+  try {
   let chats = await Chat.find();
   // console.log(chats);
   res.render("index.ejs", {chats});
+  } catch (err) {
+    next(err);
+  }
 });
 
 //New Route
-app.get("/chats/new", (req, res) => {
+app.get("/chats/new", (req, res, next) => {
+  try {
+  // throw new ExpressError(404, "Page not found ");
   res.render("new.ejs");
+  } catch (err) {
+    next(err);
+  }
 });
 
 //Create Route
-app.post("/chats", (req, res) => {
+app.post("/chats", asyncWrap (async (req, res, next) => {
   let {from, to, msg} = req.body;
   let newChat = new Chat({
     from: from,
     to : to,
     msg : msg,
-    created_at : new Date()
-  });
-  newChat.save().then((res) => {
-    console.log("chat was saved");
-  })
-  .catch ((err) => {
-    console.log(err);
-  });
+    created_at : new Date(),
+  }); 
 
+  await newChat.save();
   res.redirect("/chats");
-});
+} 
+));
+
+function asyncWrap(fn) {
+  return function(req, res, next) {
+    fn(req, res, next).catch((err) => {
+      next(err);
+    });
+  }
+}
+
+//New - Show Route
+app.get("/chats/:id", asyncWrap (async (req, res, next) => {
+  let {id} = req.params;
+  let chat = await Chat.findById(id);
+  if (!chat) {
+    next(new ExpressError(404, "chat not found"));
+  }
+  res.render("edit.ejs", {chat});
+} 
+));
 
 //edit route
-app.get("/chats/:id/edit", async (req, res) => {
+app.get("/chats/:id/edit",asyncWrap (async (req, res, next) => {
+ 
   let {id} = req.params;
   let chat = await Chat.findById(id);
   res.render("edit.ejs", {chat} );
-});
+  } ));
 
 //Update route
-app.put("/chats/:id", async (req, res) => {
+app.put("/chats/:id",asyncWrap (async (req, res, next) => {
    let {id} = req.params;
    let {msg : newMsg} = req.body;
    console.log(newMsg);
@@ -74,18 +100,22 @@ app.put("/chats/:id", async (req, res) => {
 
     console.log(updatedChat);
     res.redirect("/chats");
-});
+  } ));
 
 //Destroy 
-app.delete("/chats/:id", async (req, res) => {
+app.delete("/chats/:id",asyncWrap (async (req, res, next) => {
   let {id} =  req.params;
   let deletedChat = await Chat.findByIdAndDelete(id);
   console.log(deletedChat);
   res.redirect("/chats");
-});
+  }));
 
-app.get("/" , (req, res) => {
+app.get("/" , (req, res, next) => {
+  try {
   res.send("root is working");
+  } catch (err) {
+    next(err);
+  }
 });
 
 
@@ -100,6 +130,26 @@ app.get("/" , (req, res) => {
 
 app.get("/", (req, res) => {
   res.send("root is working");
+});
+
+const handleValidationError = (err) => {
+  console.log("This was a validation error.Please follow rules");
+  console.dir(err.message);
+  return err;
+}
+
+app.use((err, req, res, next) => {
+  console.log(err.name);
+  if(err.name === "ValidationError") {
+   err = handleValidationError(err);
+  };
+  next(err);
+});
+
+//Error handling middleware
+app.use((err, req, res, next) => {
+  let {status= 500 , message= "Some error occured"} = err;
+  res.status(status).send(message);
 });
 
 app.listen(8080, () => {
--- Mongo3/init.js
+++ Mongo3/init.js
@@ -8,7 +8,7 @@ main()
 .catch(err => console.log(err));
 
 async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/whatsapp');
+  await mongoose.connect('mongodb://127.0.0.1:27017/fakewhatsapp');
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
